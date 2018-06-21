b59bfab3fe57d2685069828b55aea09b7d143b2b
Amélie Royer <aroyer@ist.ac.at>
2018-06-21 15:57:10 +0200

    adding slurm scripts

---
+++ scripts/standard_vedai.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -N 1                            # number of nodes (usually 1)
+#SBATCH -n 4                            # number of cores
+#SBATCH --mail-user=aroyer@ist.ac.at    # send mail to user
+#SBATCH --mail-type=FAIL                # if a job fails
+#SBATCH --mem 16G                       # memory pool for all cores
+#SBATCH --time 1-00:00                  # max runtime (D-HH:MM)
+#SBATCH --partition=gpu10cards          # partition (our new GPU servers)
+#SBATCH --gres=gpu:2                    # how many GPUs to reserve
+#SBATCH --constraint=GTX1080Ti          # GPU type (unnecessary here)
+#SBATCH -o ./log/output_logs/standard-vedai.%N.%j.out     # logfile for stdout
+#SBATCH -e ./log/error_logs/standard-vedai.%N.%j.err # logfile for stderr
+
+SIZE=1024                              
+DATA='vedai'                           
+NUM_EPOCHS=1000
+NUM_GPUS=2
+DISPLAY_LOSS_EVERY_N_STEPS=250
+BATCH_SIZE=16
+
+module load tensorflow/python3/1.4.0      # enable tensorflow
+cd ${HOME}/Jupyter/ODGI                   # working directory
+
+python3 -u train_standard.py $DATA --size $SIZE --num_epochs=$NUM_EPOCHS --num_gpus=$NUM_GPUS --display_loss_very_n_steps=$DISPLAY_LOSS_EVERY_N_STEPS --batch_size=$BATCH_SIZE
