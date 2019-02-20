b87e26d029a2a823a770542e83a9a4fc03f753ca
Amelie Royer <aroyer@ist.ac.at>
2019-02-20 14:41:28 +0100

    backup validate odgi without crop slice

--- validate_odgi.py
+++ validate_odgi.py
@@ -15,6 +15,9 @@ from include import nets
 from include import viz
 from train_odgi import stage_transition, format_final_boxes
 
+import logging
+logging.getLogger('tensorflow').setLevel(logging.INFO)
+
 """Perform hyperparameter sweep for the patch extraction stage for a pre-trained 
 two stage ODGI model"""
 
@@ -35,8 +38,8 @@ test_patch_nms_threshold_sweep = [0.25, 0.5, 0.75]
 test_patch_confidence_threshold_sweep  = [0., 0.1, 0.2, 0.3, 0.4]
 test_patch_strong_confidence_threshold_sweep = [0.6, 0.7, 0.8, 0.9, 1.0]
 
-test_num_crops_sweep = [6]                                
-test_patch_nms_threshold_sweep = [0.25, 0.5]                      
+test_num_crops_sweep = [6, 1]                                
+test_patch_nms_threshold_sweep = [0.25]                      
 test_patch_confidence_threshold_sweep  = [0.1]
 test_patch_strong_confidence_threshold_sweep = [0.6]
 
@@ -59,9 +62,6 @@ for i, base_name in enumerate(['stage1', 'stage2']):
         decode_fn = tf.make_template('%s/decode' % base_name, nets.get_detection_outputs_with_groups)
     else:
         decode_fn = tf.make_template('%s/decode' % base_name, nets.get_detection_outputs)
-        
-    ## Functions
-    #def fo
     forward_pass = partial(nets.forward, forward_fn=forward_fn, decode_fn=decode_fn)
     stages.append((base_name, network_name, forward_pass, config))
     
@@ -72,22 +72,18 @@ stages[0][-1]['batch_size'] = args.batch_size
 stages[1][-1]['previous_batch_size'] = args.batch_size 
 
 
-########################################################################## Build the graph for hyperparameter test
-
-def build_graph(stages, nms_threshold, log_dir, max_test_num_crops=1):
-    """Build the graph.
-    Note that nms threshold has to be defined as a float and not a placeholder
-    """
+with tf.Graph().as_default() as graph:
+    ########################################################################## Build the graph for hyperparameter test
     # Parameters to sweep on as placeholder
+    test_patch_nms_threshold_placeholder =  tf.placeholder(tf.float32, shape=())
     test_num_crops_placeholder = tf.placeholder(tf.int32, shape=())                                    
     test_patch_confidence_threshold_placeholder = tf.placeholder(tf.float32, shape=()) 
-    test_patch_strong_confidence_threshold_placeholder = tf.placeholder(tf.float32, shape=()) 
-    # except for nms_threshold: it has to be a float in `tf.image.non_max_suppression`
+    test_patch_strong_confidence_threshold_placeholder = tf.placeholder(tf.float32, shape=())
 
     # Set-up config
-    stages[0][-1]["test_num_crops"] = max_test_num_crops     # always extract test_num_crops... 
+    stages[0][-1]["test_num_crops"] = test_num_crops_sweep[-1]     # always extract test_num_crops... 
     stages[1][-1]["test_num_crops_slice"] = test_num_crops_placeholder   # ...but only use `test_num_crops_slice` in practice  
-    stages[0][-1]["test_patch_nms_threshold"] = nms_threshold                
+    stages[0][-1]["test_patch_nms_threshold"] = test_patch_nms_threshold_placeholder                
     stages[0][-1]["test_patch_confidence_threshold"] = test_patch_confidence_threshold_placeholder
     stages[0][-1]["test_patch_strong_confidence_threshold"] = test_patch_strong_confidence_threshold_placeholder
 
@@ -98,7 +94,7 @@ def build_graph(stages, nms_threshold, log_dir, max_test_num_crops=1):
         true_fn=lambda: graph_manager.get_inputs(mode='test', verbose=0, **stages[0][3]),
         false_fn=lambda: graph_manager.get_inputs(mode='val', verbose=0, **stages[0][3]),
         name='eval_inputs')
-    
+
     # Graph  
     with tf.device('/gpu:0'):
         with tf.name_scope('dev0'):
@@ -132,10 +128,10 @@ def build_graph(stages, nms_threshold, log_dir, max_test_num_crops=1):
     with tf.name_scope('outputs'):
         eval_outputs = [image_ids, num_boxes, gt_bbs, stage2_pred_bbs, stage2_pred_confidences,
                         stage1_pred_bbs, stage1_pred_confidences, stage1_kept_out_boxes]   
-        
+
     # eval functions
-    validation_results_path = os.path.join(log_dir, 'val_output.txt')
-    test_results_path = os.path.join(log_dir, 'test_output.txt')
+    validation_results_path = os.path.join(args.log_dir, 'val_output.txt')
+    test_results_path = os.path.join(args.log_dir, 'test_output.txt')
 
     run_eval = partial(graph_manager.run_eval,
                        eval_split_placehoder=eval_split_placehoder,
@@ -144,90 +140,75 @@ def build_graph(stages, nms_threshold, log_dir, max_test_num_crops=1):
                        configuration=stages[0][-1])
     eval_validation = partial(run_eval, mode='val', results_path=validation_results_path)
     eval_test = partial(run_eval, mode='test', results_path=test_results_path)
+
     
-    # return placeholders and evaluation functions
-    return (test_num_crops_placeholder, 
-            test_patch_confidence_threshold_placeholder,
-            test_patch_strong_confidence_threshold_placeholder,
-            eval_validation,
-            eval_test)
-
-# Session creator
-gpu_mem_frac = configuration.get_defaults(config, ['gpu_mem_frac'], verbose=0)[0] 
-if gpu_mem_frac < 1.0:
-    config = tf.ConfigProto(
-        gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_mem_frac), allow_soft_placement=True)
-else:
-    config = tf.ConfigProto(allow_soft_placement=True)
-
-
-if __name__ == '__main__':
-    for test_patch_nms_threshold in test_patch_nms_threshold_sweep:    
+    ########################################################################## Start session
+    gpu_mem_frac = configuration.get_defaults(config, ['gpu_mem_frac'], verbose=0)[0] 
+    if gpu_mem_frac < 1.0:
+        session_config = tf.ConfigProto(
+            gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_mem_frac), allow_soft_placement=True)
+    else:
+        session_config = tf.ConfigProto(allow_soft_placement=True)
         
-        with tf.Graph().as_default() as graph:
-            (test_num_crops_placehoder, 
-             test_patch_confidence_threshold_placehoder,
-             test_patch_strong_confidence_threshold_placehoder,
-             eval_validation, _) = build_graph(
-                stages, test_patch_nms_threshold, args.log_dir, max_test_num_crops=test_num_crops_sweep[-1])
+    with tf.train.MonitoredSession(session_creator=tf.train.ChiefSessionCreator(
+        checkpoint_dir=args.log_dir, config=session_config)) as sess:                
+        ########################################################################## Parameters sweep
+        for test_num_crops in test_num_crops_sweep:           
+            for test_patch_nms_threshold in test_patch_nms_threshold_sweep:                                     
+                for test_patch_confidence_threshold in test_patch_confidence_threshold_sweep:
+                    for test_patch_strong_confidence_threshold in test_patch_strong_confidence_threshold_sweep:     
+                        # feed_dict
+                        additional_feed_dict = {
+                            test_num_crops_placeholder: test_num_crops,
+                            test_patch_nms_threshold_placeholder: test_patch_nms_threshold,
+                            test_patch_confidence_threshold_placeholder: test_patch_confidence_threshold,
+                            test_patch_strong_confidence_threshold_placeholder: test_patch_strong_confidence_threshold}
 
-            with tf.train.MonitoredSession(session_creator=tf.train.ChiefSessionCreator(
-                checkpoint_dir=args.log_dir, config=config)) as sess:
-                for test_num_crops in test_num_crops_sweep:                                                
-                    for test_patch_confidence_threshold in test_patch_confidence_threshold_sweep:
-                        for test_patch_strong_confidence_threshold in test_patch_strong_confidence_threshold_sweep:     
-                            # feed_dict
-                            additional_feed_dict = {
-                                test_num_crops_placehoder: test_num_crops,
-                                test_patch_confidence_threshold_placehoder: test_patch_confidence_threshold,
-                                test_patch_strong_confidence_threshold_placehoder: test_patch_strong_confidence_threshold}
+                        # Evaluate 
+                        mean_aps, eval_aps_thresholds, num_images = eval_validation(
+                            sess, 1, additional_feed_dict=additional_feed_dict, verbose=False)
+                        print('   (%s, %s, %s, %s) (%d images): %s' % (
+                            test_num_crops, test_patch_nms_threshold, test_patch_confidence_threshold, 
+                            test_patch_strong_confidence_threshold, num_images, ' - '.join(
+                                'map@%.2f = %.5f' % (thresh, mean_aps[t])
+                                for t, thresh in enumerate(eval_aps_thresholds))))
+                        viz.save_tee(args.log_dir, tee)
+
+                        # Store best parameters based on map@0.5
+                        val_map = mean_aps[0]
+                        if val_map > best_val_map[test_num_crops]:
+                            best_val_map[test_num_crops] = val_map
+                            best_test_patch_nms_threshold[test_num_crops] = test_patch_nms_threshold
+                            best_test_patch_confidence_threshold[test_num_crops] = test_patch_confidence_threshold
+                            best_test_patch_strong_confidence_threshold[
+                                test_num_crops] = test_patch_strong_confidence_threshold
+
                             
-                            # Evaluate 
-                            print('   (%s, %s, %s, %s) :' % (test_num_crops,
-                                                             test_patch_nms_threshold,
-                                                             test_patch_confidence_threshold, 
-                                                             test_patch_strong_confidence_threshold), end='')
-                            val_map = eval_validation(sess, 1, additional_feed_dict=additional_feed_dict)[0]
-                            viz.save_tee(args.log_dir, tee)
-                            
-                            # Store best parameters based on map@0.5
-                            if val_map > best_val_map[test_num_crops]:
-                                best_val_map[test_num_crops] = val_map
-                                best_test_patch_nms_threshold[test_num_crops] = test_patch_nms_threshold
-                                best_test_patch_confidence_threshold[test_num_crops] = test_patch_confidence_threshold
-                                best_test_patch_strong_confidence_threshold[
-                                    test_num_crops] = test_patch_strong_confidence_threshold
+        ########################################################################## Output test resutls for best config
+        for test_num_crops in test_num_crops_sweep: 
+            
+            test_patch_nms_threshold = best_test_patch_nms_threshold[test_num_crops]
+            test_patch_confidence_threshold = best_test_patch_confidence_threshold[test_num_crops]
+            test_patch_strong_confidence = best_test_patch_strong_confidence_threshold[test_num_crops]
 
-    ####### Outputs test accuracy for the best hyperparameters for each num_crops values
-    raise SystemExit
-    for test_num_crops in test_num_crops_sweep: 
-        test_patch_nms_threshold = best_test_patch_nms_threshold[test_num_crops]
-        test_patch_confidence_threshold = best_test_patch_confidence_threshold[test_num_crops]
-        test_patch_strong_confidence = best_test_patch_strong_confidence_threshold[test_num_crops]
-        
-        # Print best parameters
-        print('\nBest hyperparameters for %d crops: (val = %.4f)' % (test_num_crops, best_val_map[test_num_crops]))
-        print('  num_crops=', test_num_crops)
-        print('  nms_threshold=', test_patch_nms_threshold)
-        print('  tau_low=', test_patch_confidence_threshold)
-        print('  tau_high=', test_patch_strong_confidence_threshold)
+            # Print best parameters
+            print('\nBest hyperparameters for %d crops: (val = %.4f)' % (test_num_crops, best_val_map[test_num_crops]))
+            print('  num_crops =', test_num_crops)
+            print('  nms_threshold =', test_patch_nms_threshold)
+            print('  tau_low =', test_patch_confidence_threshold)
+            print('  tau_high =', test_patch_strong_confidence_threshold)
 
-        # Evaluate on the test set
-        print('Test set accuracy')
-        with tf.Graph().as_default() as graph:
-            (test_num_crops_placehoder, 
-             test_patch_confidence_threshold_placehoder,
-             test_patch_strong_confidence_threshold_placehoder,
-             _, eval_test) = build_graph(
-                stages, test_patch_nms_threshold, args.log_dir, max_test_num_crops=test_num_crops_sweep[-1])
-
+            # Evaluate on the test set
             additional_feed_dict = {
                 test_num_crops_placeholder: test_num_crops,
+                test_patch_nms_threshold_placeholder: test_patch_nms_threshold,
                 test_patch_confidence_threshold_placeholder: test_patch_confidence_threshold,
                 test_patch_strong_confidence_threshold_placeholder: test_patch_strong_confidence_threshold}
-            
-            with tf.train.MonitoredSession(session_creator=tf.train.ChiefSessionCreator(
-                checkpoint_dir=args.log_dir, config=config)) as sess:
-                eval_test(sess, 1, additional_feed_dict=additional_feed_dict)
 
-    viz.save_tee(args.log_dir, tee)
+            mean_aps, eval_aps_thresholds, num_images = eval_test(
+                sess, 1, additional_feed_dict=additional_feed_dict, verbose=False)
+            print('  evaluated %d images:' % num_images, ' - '.join(
+                'map@%.2f = %.5f' % (thresh, mean_aps[t]) 
+                for t, thresh in enumerate(eval_aps_thresholds)))
+
+        viz.save_tee(args.log_dir, tee)
